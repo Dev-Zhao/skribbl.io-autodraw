01f0c3585d36b623c701d42038d98c7228e083f2
Charlie Zhao <yczhao0916@gmail.com>
2019-07-21 03:34:10 -0400

    Set extension to only show page action during Skribblio game

--- background.js
+++ background.js
@@ -2,11 +2,12 @@ chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.sync.set({color: '#3aa757'}, function() {
       console.log("The color is green.");
     });
-    
+
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: {hostEquals: 'developer.chrome.com'},
+                pageUrl: {hostEquals: 'skribbl.io', schemes: ['https']},
+                css: ["#canvasGame"]
             })
             ],
                 actions: [new chrome.declarativeContent.ShowPageAction()]
