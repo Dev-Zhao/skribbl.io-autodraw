d01e7f3c1d89557f8a7389a2911783d9fabf7a9e
Charlie Zhao <yczhao0916@gmail.com>
2019-08-08 02:33:59 -0400

    Reorganize code and set overlay to hide when dragged item is not in the page

--- content_scripts/main.js
+++ content_scripts/main.js
@@ -1,24 +1,71 @@
-var gameCanvas = document.getElementById('canvasGame');
+// This is the canvas that you draw on for Skribbl.io
+let gameCanvas = document.getElementById('canvasGame');
+// The container of the game canvas
+let canvasContainer = document.getElementById('containerCanvas');
 
-var instructionOverlay = document.createElement("div");
-instructionOverlay.id = "instructionOverlay";
-instructionOverlay.style.backgroundImage = "url('" + chrome.extension.getURL("images/instruction_overlay_background.gif") + "')";
-instructionOverlay.classList.add("hide");
-document.getElementById("containerCanvas").prepend(instructionOverlay);
+let setUpInstructionOverlay = function () {
+    let instructionOverlay = document.createElement('div');
+    instructionOverlay.id = 'instruction-overlay';
+    instructionOverlay.style.backgroundImage = 'url("' + chrome.extension.getURL("images/instruction_overlay_background.gif") + '")';
+    canvasContainer.prepend(instructionOverlay);
 
-var instructionOverlayText = document.createElement("h1");
-instructionOverlayText.appendChild(document.createTextNode("Drop an image here to draw!"));
-instructionOverlayText.style.textAlign = "center";
-instructionOverlay.appendChild(instructionOverlayText);
+    let instructionOverlayText = document.createElement('h1');
+    instructionOverlayText.appendChild(document.createTextNode('Drop an image here to draw!'));
+    instructionOverlay.appendChild(instructionOverlayText);
+};
 
-document.addEventListener("dragstart", function (event) {
-    // show overlay and hide game canvas
-    instructionOverlay.classList.remove("hide");
-    gameCanvas.style.opacity = 0;
+let showInstructionOverlay = function () {
+    canvasContainer.classList.add('show-instruction-overlay');
+};
+
+let hideInstructionOverlay = function () {
+    canvasContainer.classList.remove('show-instruction-overlay');
+};
+
+// Show overlay when 'dragenter' or 'dragover' occurs on the entire document (every element)
+// 'dragenter' - something is dragged and enters any element on the page
+// 'dragover' - something is dragged and it's over an element on the page, will occur every few hundred milliseconds for as long as the dragged item is over the element
+let showOverlay = true;
+let timeout = -1;
+document.addEventListener('dragenter', function (event) {
+    showOverlay = true;
+    showInstructionOverlay();
 });
 
-document.addEventListener("dragend", function (event) {
-    // Hide overlay and show canvas
-    instructionOverlay.classList.add("hide");
-    gameCanvas.style.opacity = 1;
+document.addEventListener('dragover', function (event) {
+    showOverlay = true;
+    showInstructionOverlay();
 });
+
+// If the dragged item leaves an element, we set showOverlay to false, and check if it's still false after a small period of time.
+// If it's still false then the dragged item has left the page and we hide the overlay.
+// 'Detect Drag event entering and leaving window' - https://stackoverflow.com/a/14248483
+document.addEventListener('dragleave', function (event) {
+    showOverlay = false;
+    
+    // Clear existing timeout before setting next one. This is because if multiple dragleave happens in a short period of time, 
+    // we would have many unnecessary timeouts, since they are all doing the samething.
+    clearTimeout(timeout);
+
+    timeout = setTimeout(function () {
+        if (!showOverlay) {
+            hideInstructionOverlay();
+        }
+    }, 200);
+});
+
+gameCanvas.addEventListener('dragover', function (event) {
+    // Must stop the default action of 'dragover' for 'drop' event to fire
+    event.preventDefault();
+});
+
+gameCanvas.addEventListener('drop', function (event) {
+
+    // When the dragged item is dropped into the game canvas, we hide the overlay
+    hideInstructionOverlay();
+
+    // Prevent browser from loading the file that you drop
+    event.preventDefault();
+});
+
+setUpInstructionOverlay();
