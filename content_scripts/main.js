a55410922de23d82d9ab223fe148ea5a5ebd177d
Charlie Zhao <yczhao0916@gmail.com>
2019-08-14 21:52:29 -0400

    Reorganize code

--- content_scripts/main.js
+++ content_scripts/main.js
@@ -1,9 +1,9 @@
-// This is the canvas that you draw on for Skribbl.io
+// The canvas that you draw on for Skribbl.io
 let gameCanvas = document.getElementById('canvasGame');
 // The container of the game canvas
 let canvasContainer = document.getElementById('containerCanvas');
 
-let setUpInstructionOverlay = function () {
+let createInstructionOverlay = function () {
     let instructionOverlay = document.createElement('div');
     instructionOverlay.id = 'instruction-overlay';
     instructionOverlay.style.backgroundImage = 'url("' + chrome.extension.getURL("images/instruction_overlay_background.gif") + '")';
@@ -22,59 +22,90 @@ let hideInstructionOverlay = function () {
     canvasContainer.classList.remove('show-instruction-overlay');
 };
 
-let drawImage = function (imgSrc){
+// 'Load an image using Promise()' - https://stackoverflow.com/a/52060802
+// 'Promise' - https://javascript.info/promise-basics
+let loadImage = function (imgSrc) {
+    return new Promise(function (resolve, reject) {
+        let img = new Image();
 
+        img.addEventListener('load', function () {
+            resolve(img);
+        });
+
+        img.addEventListener('error', reject);
+
+        // When you draw on a canvas with any data loaded from another origin without CORS, canvas becomes tainted.
+        // Any attempts to retrieve image data from canvas will cause an exception.
+        // 'Allowing cross-origin use of images and canvas' - 'https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image
+        img.crossOrigin = "Anonymous"; // Fetch image using CORS without credentials - https://developer.mozilla.org/en-US/docs/Web/HTML/Element/img#Attributes
+        img.src = imgSrc;
+    });
 };
 
-// Show overlay when 'dragenter' or 'dragover' occurs on the entire document (every element)
-// 'dragenter' - something is dragged and enters any element on the page
-// 'dragover' - something is dragged and it's over an element on the page, will occur every few hundred milliseconds for as long as the dragged item is over the element
-let showOverlay = false;
-let timeout = -1;
-document.addEventListener('dragenter', function (event) {
-    showOverlay = true;
-    showInstructionOverlay();
-});
+let drawImage = function (imgSrc) {
+    loadImage(imgSrc).then(function (img) {
+    });
+};
 
-document.addEventListener('dragover', function (event) {
-    showOverlay = true;
-    showInstructionOverlay();
-});
+let createDocumentDragEventsHandler = function () {
+    // Using a closure because it makes variables only accessible by the inner function and it stays alive between function calls
+    // 'Closure' - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures#Closure
+    let showOverlay = false;
+    let timeout = null;
 
-// If the dragged item leaves an element, we set showOverlay to false, and check if it's still false after a small period of time.
-// If it's still false then the dragged item has left the page and we hide the overlay.
-// 'Detect Drag event entering and leaving window' - https://stackoverflow.com/a/14248483
-document.addEventListener('dragleave', function (event) {
-    showOverlay = false;
-    
-    // Clear existing timeout before setting next one. This is because if multiple dragleave happens in a short period of time, 
-    // we would have many unnecessary timeouts, since they are all doing the samething.
-    clearTimeout(timeout);
-
-    timeout = setTimeout(function () {
-        if (!showOverlay) {
-            hideInstructionOverlay();
+    // 'Detect Drag event entering and leaving window' - https://stackoverflow.com/a/14248483
+    return function documentDragEventsHandler(event) {
+        // Show overlay when 'dragenter' or 'dragover' occurs on the entire document (every element)
+        // 'dragenter' - something is dragged and enters any element on the page
+        // 'dragover' - something is dragged and it's over an element on the page, will occur once every few hundred milliseconds while the dragged item is over the element
+        if (event.type == 'dragenter' || event.type == 'dragover') {
+            showOverlay = true;
+            showInstructionOverlay();
         }
-    }, 200);
-});
+        // If 'dragleave' occurs, it could mean that the dragged item has left the page. We set showOverlay to false, and check if it's still false after a small period of time. 
+        // If no 'dragenter' or 'dragover' events occur during this period, it will be false, which will only be the case if the dragged item has left the page
+        else if (event.type == 'dragleave') {
+            showOverlay = false;
 
-gameCanvas.addEventListener('dragover', function (event) {
-    // Must stop the default action of 'dragover' for 'drop' event to fire
-    event.preventDefault();
-});
+            // Clear existing timeout before setting next one. This is because if multiple dragleave happens in a short period of time, 
+            // we would have many unnecessary timeouts, since they are all doing the samething.
+            clearTimeout(timeout);
 
-gameCanvas.addEventListener('drop', function (event) {
-    let imgSrc = imageSourceHelper.getSrcFromImgFile(event.dataTransfer) || imageSourceHelper.getSrcFromImgElement(event.dataTransfer);
+            timeout = setTimeout(function () {
+                if (!showOverlay) {
+                    hideInstructionOverlay();
+                }
+            }, 200);
+        }
+    };
+};
 
-    if (imgSrc){
-        console.log(imgSrc);
+let gameCanvasDragEventsHandler = function (event) {
+    if (event.type == 'dragover') {
+        // Must stop the default action of 'dragover' for 'drop' event to fire
+        event.preventDefault();
     }
+    else if (event.type == 'drop') {
+        // When the dragged item is dropped into the game canvas, hide the overlay
+        hideInstructionOverlay();
 
-    // When the dragged item is dropped into the game canvas, hide the overlay
-    hideInstructionOverlay();
+        let imgSrc = imageHelper.getSrcFromImgFile(event.dataTransfer) || imageHelper.getSrcFromImgElement(event.dataTransfer);
 
-    // Prevent browser from loading the file that is dropped
-    event.preventDefault();
-});
+        if (imgSrc) {
+            drawImage(imgSrc);
+        }
 
-setUpInstructionOverlay();
+        // Prevent browser from loading the file that is dropped
+        event.preventDefault();
+    }
+};
+
+let documentDragEventsHandler = createDocumentDragEventsHandler();
+document.addEventListener('dragenter', documentDragEventsHandler);
+document.addEventListener('dragover', documentDragEventsHandler);
+document.addEventListener('dragleave', documentDragEventsHandler);
+
+gameCanvas.addEventListener('dragover', gameCanvasDragEventsHandler);
+gameCanvas.addEventListener('drop', gameCanvasDragEventsHandler);
+
+createInstructionOverlay();
