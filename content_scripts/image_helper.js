d0ce168218d64b27c4f1e8a68e8e766d6b4981c2
Yu Chen Zhao <yczhao0916@gmail.com>
2019-11-10 01:01:59 -0500

    Move some functions from image helper to data transfer helper

--- content_scripts/image_helper.js
+++ content_scripts/image_helper.js
@@ -1,45 +1,32 @@
 let imageHelper = {
-    // User may drag and drop an image from their computer
-    getSrcFromImgFile: function (dataTransfer) {
-        let imgSrc = null;
+    // 'Load an image using Promise()' - https://stackoverflow.com/a/52060802
+    // 'Promise' - https://javascript.info/promise-basics
+    loadImage: function (imgSrc) {
+        return new Promise(function (resolve, reject) {
+            let img = new Image();
 
-        // Check if there is a file
-        if (dataTransfer.files.length) {
-            // Check if file is an image
-            if (dataTransfer.files[0].type.startsWith('image')) {
-                // Create URL so it can be used with <img> elements
-                // 'How to load image from user' - https://stackoverflow.com/a/51760964
-                // 'Using URL.createObjectURL()' - https://chrisrng.svbtle.com/using-url-createobjecturl
-                imgSrc = URL.createObjectURL(dataTransfer.files[0]);
-            }
-        }
+            img.addEventListener('load', function () {
+                resolve(img);
+            });
 
-        return imgSrc;
+            img.addEventListener('error', reject);
+
+            // When you draw on a canvas with any data loaded from another origin without CORS, canvas becomes tainted.
+            // Any attempts to retrieve image data from canvas will cause an exception.
+            // 'Allowing cross-origin use of images and canvas' - 'https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image
+            img.crossOrigin = "Anonymous"; // Fetch image using CORS without credentials - https://developer.mozilla.org/en-US/docs/Web/HTML/Element/img#Attributes
+            img.src = imgSrc;
+        });
     },
 
-    // User may drag and drop an image from the web
-    getSrcFromImgElement: function (dataTransfer) {
-        let imgSrc = null;
+    scaleImage: function (img, options) {
+        // Setting options or using default values if they don't exist
+        let size = {
+            width: options.width || 800,
+            height: options.height || 600,
+        };
+        let scaleMode = options.scaleMode || 'scaleToFit';
 
-        let data = dataTransfer.getData('text/html');
-        if (data) {
-            // Although the data is in 'text/HTML' format, it's still just text 
-            // This converts the text into DOM elements we can interact with
-            // 'Converting HTML string into DOM elements' - https://stackoverflow.com/a/3104251
-            let wrapper = document.createElement('div');
-            wrapper.innerHTML = data;
-
-            // We extract the element that was just converted from text and check if it's an <img> element
-            let element = wrapper.firstChild;
-            if (element && element.tagName == "IMG") {
-                imgSrc = element.src;
-            }
-        }
-
-        return imgSrc;
-    },
-
-    scaleImage: function (img, size, scaleMode) {
         let canvas = document.createElement('canvas');
 
         // 'Scaling an image to fit on canvas' - https://stackoverflow.com/a/23105310
@@ -88,40 +75,60 @@ let imageHelper = {
         let dx = (canvas.width - scaledImageWidth) / 2;
         let dy = (canvas.height - scaledImageHeight) / 2;
 
+        // Draw the image on the canvas, this is where the scaling happens
+        // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/drawImage
         let canvasContext = canvas.getContext('2d');
         canvasContext.drawImage(img, dx, dy, scaledImageWidth, scaledImageHeight);
 
         return canvasContext.getImageData(0, 0, canvas.width, canvas.height);
-    }
+    },
+
+    getPixelColor: function(imageData, x, y){
+        const data = imageData.data;
+
+        // Suppose you have a 2x2 image:
+        // RED PIXEL  (x:0, y:0) |       GREEN PIXEL (x:1, y:0)
+        // BLUE PIXEL (x:0, y:1) | TRANSPARENT PIXEL (x:1, y:1)
+        // The data array is 1-dimensional, and it looks like this:
+        // [ 255, 0, 0, 255, 0, 255, 0, 255, 0, 0, 255, 255, 0, 0, 0, 0 ]  
+        // |   1ST PIXEL   |   2ND PIXEL   |   3RD PIXEL   | 4TH  PIXEL |
+        // Each pixel has 4 values which corresponds to rgba
+        // Notice the index for the first pixel's values starts at 0, second pixel: 4, third: 8, fourth: 12
+        // This can be calculated using the formula: 4*y*imageWidth + 4*x
+        let i = 4 * y * imageData.width + 4 * x; // r: data[i], g: data[i+1], b: data[i+2], a: data[i+3]
+
+        return new Color(data[i], data[i+1], data[i+2], data[i+3]);
+    },
+
+    // 'Get average color from area of image' - https://stackoverflow.com/a/44557266
+    // To get the pixel data (array) from an area of the image, the solution uses: context.getImageData(x, y, width, height).data
+    // This is very slow, it's much faster to get the pixel data (array) of the entire image and use indexes to get the values for the specific pixels you want
+    getAverageColor: function (imageData, startX, startY, width, height) {
+        let totals = { r: 0, g: 0, b: 0, weight: 0, numPixels: 0};
+
+        for (let y = startY; y < startY + height; y++) {
+            for (let x = startX; x < startX + width; x++) {
+                let color = this.getPixelColor(imageData, x, y);
+
+                // Use the alpha channel as the weight, the more transparent the pixel, the less we care about its rgb values
+                let weight = color.a / 255;
+                totals.r += color.r * weight;
+                totals.g += color.g * weight;
+                totals.b += color.b * weight;
+                totals.weight += weight;
+                totals.numPixels++;         
+            }
+        }
+
+        let averageColor = new Color(
+            // The | operator stands for bitwise OR, OR 0 will truncate any decimals
+            // 'Using bitwise OR 0 to floor a number' - https://stackoverflow.com/questions/7487977/using-bitwise-or-0-to-floor-a-number
+            totals.r / totals.weight | 0, // r
+            totals.g / totals.weight | 0, // g
+            totals.b / totals.weight | 0, // b
+            totals.weight / totals.numPixels, // a
+        );
+
+        return averageColor;
+    },
 };
-
-/*
-        let imgProcessor = createImageProcessor(img);
-        let imageData = imgProcessor.processImage({scaleImage: true, scaleMode: 'scaleToFill', size: {width: 500, height: 300}});
-        let cache = {};
-        let colorPalette = createColorPalette(toolbar.getColors(), cache);
-        let data = imageData.data;
-
-        let rect = gameCanvas.getBoundingClientRect();
-        let x = 0;
-        let y = 0;
-        toolbar.setBrushThickness(20.2);
-        for (let i = 0; i < 1000; i += 4) {
-            let currentColor = { r: data[i], g: data[i+1], b: data[i+2]};
-            let closestColor = colorPalette.getNearestColor(currentColor);
-
-            toolbar.setColor(JSON.stringify(closestColor));
-
-
-            let evt = new MouseEvent ("mousedown", {bubbles: true, clientX: rect.x+x, clientY: rect.y+y, button: 0});
-            gameCanvas.dispatchEvent(evt);
-            let evt2 = new MouseEvent ("mouseup", {bubbles: true, clientX: rect.x+x, clientY: rect.y+y, button: 0});
-            gameCanvas.dispatchEvent(evt2);
-
-           x++;
-           if (x > 800){
-               y++;
-               x = 0;
-           }
-       }
-*/
