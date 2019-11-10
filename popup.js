fe39dff9b5f59ae606836d9f8a325d161bbbc47d
Yu Chen Zhao <yczhao0916@gmail.com>
2019-11-10 01:04:22 -0500

    Add popup for user to choose options

--- popup.js
+++ popup.js
@@ -1,15 +1,60 @@
-let changeColor = document.getElementById('changeColor');
+var drawModeDotsRadio = document.getElementById("drawModeDotsRadio");
+var drawModeLinesRadio = document.getElementById("drawModeLinesRadio");
+var brushNumRangeInput = document.getElementById("brushNumRangeInput");
+var brushNumIndicator = document.getElementById('brushNumIndicator');
+var saveButton = document.getElementById("saveButton");
 
-chrome.storage.sync.get('color', function (data) {
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color);
+function updateDrawModeRadioButtons(drawMode){
+    switch (drawMode){
+        case "Dots":
+            drawModeDotsRadio.checked = true;
+            break;
+        case "Lines":
+            drawModeLinesRadio.checked = true;
+            break;
+    }
+}
+
+function updatebrushNum(brushNum){
+    brushNumRangeInput.value = brushNum;
+
+    switch(brushNum){
+        case 0:
+            brushNumIndicator.innerHTML = "Small";
+            break;
+        case 1:
+            brushNumIndicator.innerHTML = "Medium";
+            break;
+        case 2:
+            brushNumIndicator.innerHTML = "Large";
+            break;
+        case 3:
+            brushNumIndicator.innerHTML = "Extra Large";
+            break;
+    }
+}
+
+Promise.all([storage.getData("drawMode"), storage.getData("brushNum")]).then((data) => {
+    let drawMode = data[0];
+    let brushNum = data[1];
+
+    updateDrawModeRadioButtons(drawMode);
+
+    updatebrushNum(brushNum);
+}).catch((errors) => {
+    console.log(errors);
 });
 
-changeColor.onclick = function (element) {
-    let color = element.target.value;
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            { code: 'document.body.style.backgroundColor = "' + color + '";' });
+
+brushNumRangeInput.oninput = function(){
+    updatebrushNum(parseInt(brushNumRangeInput.value));
+};
+
+saveButton.onclick = function(){
+    var drawMode = document.querySelector('input[name="drawModeRadios"]:checked').value;
+    var brushNum = parseInt(brushNumRangeInput.value);
+
+    storage.setData({drawMode: drawMode, brushNum: brushNum}).catch((err) => {
+        console.log(err);
     });
 };
