f2f96e465c610a58263098e8c0b9cdbc57535e5a
Yu Chen Zhao <yczhao0916@gmail.com>
2019-11-10 00:54:51 -0500

    Update background script to set default options values

--- background.js
+++ background.js
@@ -1,12 +1,20 @@
 chrome.runtime.onInstalled.addListener(function() {
+    chrome.storage.sync.set({
+        drawMode: "Lines", 
+        brushNum: 1
+    }, function(){ 
+        if (chrome.runtime.lastError) {
+            console.warn("Whoops.. " + chrome.runtime.lastError.message);
+        }
+    });
+
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: {hostEquals: 'skribbl.io', schemes: ['https']},
-                css:['#screenGame']
-            })
-            ],
-                actions: [new chrome.declarativeContent.ShowPageAction()]
+                pageUrl: {hostEquals: 'skribbl.io', schemes: ['https']}
+            })],
+            
+            actions: [new chrome.declarativeContent.ShowPageAction()]
         }]);
     });
 });
